21912345345ed69ea648c0167fc962924e7bb827
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2021-04-02 13:46:58 +0600

    Basic firebase installation

--- app/src/main/java/com/example/diunotessharingportal/UploadNotes.java
+++ app/src/main/java/com/example/diunotessharingportal/UploadNotes.java
@@ -1,17 +1,25 @@
 package com.example.diunotessharingportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UploadNotes extends AppCompatActivity {
 
 
     EditText dept,course,topic, des;
     Button btn;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,18 @@ public class UploadNotes extends AppCompatActivity {
         des = findViewById(R.id.editTextTextPersonName4);
         btn = findViewById(R.id.button);
 
+        reference= FirebaseDatabase.getInstance().getReference();
+        reference.child("Users").setValue("Our first message").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()){
+                    Toast.makeText(UploadNotes.this,"Data stored", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(UploadNotes.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
