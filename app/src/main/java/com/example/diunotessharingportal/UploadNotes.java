d12dc500d55b38166b4b9e440edf4bc7e6c5ed59
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2021-04-02 20:02:37 +0600

    Firebase is running

--- app/src/main/java/com/example/diunotessharingportal/UploadNotes.java
+++ app/src/main/java/com/example/diunotessharingportal/UploadNotes.java
@@ -4,15 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class UploadNotes extends AppCompatActivity {
 
@@ -20,6 +25,7 @@ public class UploadNotes extends AppCompatActivity {
     EditText dept,course,topic, des;
     Button btn;
     DatabaseReference reference;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +36,13 @@ public class UploadNotes extends AppCompatActivity {
         topic = findViewById(R.id.editTextTextPersonName3);
         des = findViewById(R.id.editTextTextPersonName4);
         btn = findViewById(R.id.button);
+        textView=findViewById(R.id.textView3);
+
+
 
         reference= FirebaseDatabase.getInstance().getReference();
-        reference.child("Users").setValue("Our first message").addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
 
-                if (task.isSuccessful()){
-                    Toast.makeText(UploadNotes.this,"Data stored", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(UploadNotes.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +52,41 @@ public class UploadNotes extends AppCompatActivity {
                 String topicv = topic.getText().toString();
                 String desv = des.getText().toString();
 
+                Model model = new Model(deptv,coursev,topicv,desv);
 
+
+
+
+                reference.child("Users").push().setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(UploadNotes.this,"Data stored", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(UploadNotes.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+            }
+        });
+
+
+        reference.child("Users").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                if (task.isSuccessful()){
+                    DataSnapshot snapshot = task.getResult();
+                    textView.setText(snapshot.toString());
+                }
+                else{
+                    Toast.makeText(UploadNotes.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
